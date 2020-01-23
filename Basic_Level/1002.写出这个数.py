ac7af69c967c56fb3d92bdd6b0ebb405a78a0af7
escape-plan <1184772379@qq.com>
2020-01-23 23:59:05 +0800

    Update 1002.写出这个数.py
    代码优化

--- Basic_Level/1002.写出这个数.py
+++ Basic_Level/1002.写出这个数.py
@@ -1,12 +1,8 @@
 n = input()
+
 sum = 0
 for i in n:
     sum += eval(i)
 
 l = ['ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba','jiu']
-
-for k, i in enumerate(str(sum)):
-    if k == 0:
-        print(l[int(i)], end = '')
-    else:
-        print(" "+ l[int(i)], end = '')
+print(' '.join(l[int(i)] for i in str(sum)))
